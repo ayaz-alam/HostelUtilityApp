0cce3971daaa8311ac611b582ff9264e617c5eeb
Ayaz <ayazalam922@gmail.com>
2019-09-30 02:04:55 +0530

    minor changes

--- app/src/main/java/com/code_base_update/presenters/IComplaintPresenter.java
+++ app/src/main/java/com/code_base_update/presenters/IComplaintPresenter.java
@@ -1,14 +1,9 @@
 package com.code_base_update.presenters;
-
-
-import android.content.Context;
-
 import com.code_base_update.beans.ComplaintBean;
 import com.code_base_update.view.IComplaintView;
 
 public interface IComplaintPresenter extends IBasePresenter<IComplaintView>{
     void loadDomain();
     void onDomainSelected(String domain);
-
     void registerComplaint(ComplaintBean complaintToRegister);
 }
