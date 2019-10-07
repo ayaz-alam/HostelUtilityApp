e41efeceac8270042f09bae7300b4a9104a9a97c
Ayaz Alam <ayazalam922@gmail.com>
2019-10-07 09:34:00 +0530

    Registration Activity implemented

--- app/src/main/java/com/code_base_update/beans/Student.java
+++ app/src/main/java/com/code_base_update/beans/Student.java
@@ -2,8 +2,6 @@ package com.code_base_update.beans;
 
 public class Student {
 
-    //TODO @Kanika add all the parameters here
-
     private String studentName, FatherName, Email, mobileNo ;
     private String enrollNo, adharNo, whatsappNo, room, address, guardiaName;
 
